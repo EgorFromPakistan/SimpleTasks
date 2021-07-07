4cbfaed1df8409a72566d3577c00f44f0f17456f
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-08 00:01:24 +0300

    Task4

---
+++ src/module_1/Task4.java
@@ -0,0 +1,13 @@
+/**
+ * Вывести на консоль 5 раз (каждый раз с новой строки) своё имя.
+ */
+
+package module_1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Egor");
+        }
+    }
+}
