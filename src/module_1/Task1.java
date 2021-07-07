fd641dff8ce0edcb8ffdf0d4c7b75fdf3422b7b6
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-07 23:53:48 +0300

    Task1

--- src/module_1/Task1.java
+++ src/module_1/Task1.java
@@ -1,7 +1,12 @@
+/**
+ * Вывести на экран своё имя
+ */
+
 package module_1;
 
 public class Task1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Egor");
     }
 }
