0641707f8116eff30c33412b63c95bb5926d0984
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-07 23:59:41 +0300

    Task3

---
+++ src/module_1/Task3.java
@@ -0,0 +1,12 @@
+/**
+ * Объявить переменную типа int, присвоить ей год своего рожения и вывести на экран.
+ */
+
+package module_1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int age = 1999;
+        System.out.println(age);
+    }
+}
