88e8b5b69e1454ed775d176f51f7c06410bda7cc
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-08 22:37:49 +0300

    Task7

---
+++ src/module_1/Task8.java
@@ -0,0 +1,11 @@
+/**
+ * Сейчас 3126 год. Мой друг родился 8 лет назад. Написать программу, выводящую на экран год рождения моего друга.
+ */
+
+package module_1;
+
+public class Task8 {
+    public static void main(String[] args) {
+        System.out.println("My friend's year of birth= " + (3126 - 8));
+    }
+}
