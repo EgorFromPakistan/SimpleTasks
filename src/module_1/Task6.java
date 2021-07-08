0cba2da19f5e41b92f716513e37b35b9262c3da0
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-08 22:33:14 +0300

    Task6

---
+++ src/module_1/Task6.java
@@ -0,0 +1,13 @@
+/**
+ * Напиши программу, которая в методе main объявляет такие переменные: name типа String, age типа int и city типа String.
+ */
+
+package module_1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String city;
+    }
+}
