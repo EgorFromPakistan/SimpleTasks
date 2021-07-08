2447676dc1ee0d031ed27e475a8645168edb4deb
e.gutko@pixelplex.io <e.gutko@pixelplex.io>
2021-07-08 22:35:50 +0300

    Task7

---
+++ src/module_1/Task7.java
@@ -0,0 +1,13 @@
+/**
+ * Объяви две переменные типа int, присвой им любые числовые значения, выведи на экран их сумму.
+ */
+
+package module_1;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int firstValue = 1;
+        int secondValue = 2;
+        System.out.println("Sum = " + (firstValue + secondValue));
+    }
+}
